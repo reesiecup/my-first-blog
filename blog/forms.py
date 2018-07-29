6912f911c44e8dd05b537aed77cd63dd0b18d61a
reesiecup <reesetou@gmail.com>
2018-07-29 18:20:11 -0500

    attempt #1 to add widget for the post form

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,18 @@
 from django import forms
+from tinymce import TinyMCE
 from .models import Post, Comment
 
-class PostForm(forms.ModelForm):
 
-    class Meta:
-        model = Post
-        fields = ('title', 'text',)
+class TinyMCEWidget(TinyMCE):
+    def use_required_attribute(self, *args):
+        return False
+
+class PostForm(forms.ModelForm):
+	text = forms.CharField(widget=TinyMCEWidget(attrs={'cols': 80, 'rows': 15}))
+
+	class Meta:
+		model = Post
+		fields = ('title', 'text',)
 
 class CommentForm(forms.ModelForm):
 
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils import timezone
-
+from tinymce import models as tinymce_models
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = tinymce_models.HTMLField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
